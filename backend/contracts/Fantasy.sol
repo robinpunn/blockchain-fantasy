7bb00cca35b5cf6bafe030e43be6685fbccc46b7
Robin Punnoose <robin.punnoose@protonmail.com>
2023-07-19 16:32:12 -0500

    updated contract

--- backend/contracts/Fantasy.sol
+++ backend/contracts/Fantasy.sol
@@ -9,49 +9,101 @@ contract FantasyFootballContract {
     }
 
     struct Season {
+        uint id;
         address commissioner;
-        uint participants;
+        mapping(address => bool) whitelist;
         mapping(address => Player) players;
+        uint buyIn;
         uint prizePool;
         bool started;
         bool complete;
     }
 
-    mapping(address => Season) seasons;
+    uint private seasonCounter;
+    Season[] seasons;
 
-    function startSeason() public {
-        // Add logic to start a new season
-        require(!seasons[msg.sender].started, "Season already started.");
+    event SeasonStarted(uint seasonId, address commissioner);
+    event MemberJoined(uint seasonId, address member);
+    event PlayerWin(uint seasonId, address member, uint addedWinning);
+    event SeasonCompleted(uint seasonId, address winner, uint totalWinnings);
 
-        seasons[msg.sender].commissioner = msg.sender;
-        seasons[msg.sender].started = true;
+    modifier onlyWhitelisted(uint _seasonId, address _address) {
+        require(
+            seasons[_seasonId].whitelist[_address],
+            "Address not whitelisted"
+        );
+        _;
     }
 
-    function buyIn() public payable {
-        // Add logic to handle participant buy-ins
-        require(seasons[msg.sender].started, "Season not started.");
-        require(!seasons[msg.sender].complete, "Season already completed.");
-
-        Season storage currentSeason = seasons[msg.sender];
-        require(msg.value > 0, "Buy-in amount must be greater than zero.");
+    modifier onlyCommissioner(uint _seasonId) {
         require(
-            currentSeason.players[msg.sender].id == address(0),
-            "Already bought in."
+            msg.sender == seasons[_seasonId].commissioner,
+            "Only commissioner can perform this action"
+        );
+        _;
+    }
+
+    function addSeason(uint _buyIn) external {
+        uint newSeasonId = seasonCounter;
+        Season storage newSeason = seasons[newSeasonId];
+        newSeason.id = newSeasonId;
+        newSeason.buyIn = _buyIn * 1 ether;
+        newSeason.commissioner = msg.sender;
+        newSeason.started = true;
+        seasonCounter++;
+
+        emit SeasonStarted(newSeasonId, msg.sender);
+    }
+
+    function addToWhitelist(
+        uint _seasonId,
+        address _address
+    ) external onlyCommissioner(_seasonId) {
+        seasons[_seasonId].whitelist[_address] = true;
+    }
+
+    function removeFromWhitelist(
+        uint _seasonId,
+        address _address
+    ) external onlyCommissioner(_seasonId) {
+        delete seasons[_seasonId].whitelist[_address];
+    }
+
+    function buyIn(
+        uint _seasonId
+    ) external payable onlyWhitelisted(_seasonId, msg.sender) {
+        Season storage season = seasons[_seasonId];
+        require(season.started, "Season has not started yet");
+
+        require(msg.value == season.buyIn, "Incorrect buy-in amount");
+
+        Player storage player = season.players[msg.sender];
+        require(
+            player.id == address(0),
+            "Player has already joined the season"
         );
 
-        currentSeason.players[msg.sender] = Player({
-            id: msg.sender,
-            paid: true,
-            winnings: 0
-        });
-        currentSeason.participants++;
+        player.id = msg.sender;
+        player.paid = true;
+        season.prizePool += msg.value;
+
+        emit MemberJoined(_seasonId, msg.sender);
     }
 
-    function completeSeason() public {
-        // Add logic to complete a season
-        require(seasons[msg.sender].started, "Season not started.");
-        require(!seasons[msg.sender].complete, "Season already completed.");
+    function addWinnings(
+        uint _seasonId,
+        address _player,
+        uint _winnings
+    ) external onlyCommissioner(_seasonId) {
+        Season storage season = seasons[_seasonId];
+        require(season.started, "Season has not started yet");
 
-        seasons[msg.sender].complete = true;
+        Player storage player = season.players[_player];
+        require(player.id != address(0), "Player is not part of the season");
+
+        player.winnings += _winnings;
+        season.prizePool -= _winnings;
+
+        emit PlayerWin(_seasonId, _player, _winnings);
     }
 }
