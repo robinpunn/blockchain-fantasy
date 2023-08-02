e8cbfb2d868a86c4cce4c3e42536c8aecb9b8623
Robin Punnoose <robin.punnoose@protonmail.com>
2023-08-02 16:33:29 -0500

    updated addLeague component, integrated with wagmi

--- frontend/components/addLeague.tsx
+++ frontend/components/addLeague.tsx
@@ -1,15 +1,52 @@
 import React, {useState} from 'react'
+import {useContractWrite, useContractEvent} from 'wagmi';
+import { LogDescription, parseEther } from 'ethers';
+import Fantasy from '../artifacts/contracts/Fantasy.sol/Fantasy.json';
 import styles from '../styles/Contracts.module.css'
 
+const contractAddress = "0x8C486D366701f03b30a8106410ed98eF1660DBa4"
+
 const AddLeague = () => {
-  const [buyIn, setBuyin] = useState(null);
+  const [buyIn, setBuyin] = useState('');
+
+  const {write, isLoading, isSuccess} = useContractWrite({
+    address: contractAddress,
+    abi: Fantasy.abi,
+    functionName: 'addSeason'
+  })
+
+  useContractEvent({
+    address: contractAddress,
+    abi: Fantasy.abi,
+    eventName: 'SeasonStarted',
+    listener: (log) => {
+      const seasonId = log[0];
+      const commissioner = log[1];
+      alert(`Season ${seasonId.toString()} started by commissioner ${commissioner}!`);
+      console.log(log)
+    },
+  });
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    if (!isLoading) {
+      await write({ args: [parseEther(buyIn)]})
+    }
+  }
 
   return (
-    <form className={styles.search}>
-        <p>Add a League:</p>
-        <input type="text" placeholder='Enter Buy In'/>
-        <button>Add League</button>
-    </form>
+      <form className={styles.search} onSubmit={handleSubmit}>
+          <label htmlFor='buyIn'>Add A League:</label>
+          <input
+            type="text"
+            placeholder='Enter Buy In'
+            id="buyIn"
+            value={buyIn}
+            onChange={(event) => setBuyin(event.target.value)}
+            required
+          />
+          <button>Add League</button>
+      </form>
   )
 }
 
--- frontend/components/findLeague.tsx
+++ frontend/components/findLeague.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/Contracts.module.css'
 
 const FindLeague = () => {
+  const [id,setId] = useState('');
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+  }
+
   return (
-    <div className={styles.search}>
-        <p>Find your league:</p>
-        <input type="text" placeholder='Enter League Id' />
-        <button>Find</button>
-    </div>
+    <form className={styles.search}>
+          <label htmlFor='buyIn'>Find Your League:</label>
+          <input
+            type="text"
+            placeholder='Enter League Id'
+            id="buyIn"
+            value={id}
+            onChange={(event) => setId(event.target.value)}
+            required
+          />
+          <button>Find</button>
+      </form>
   )
 }
 
