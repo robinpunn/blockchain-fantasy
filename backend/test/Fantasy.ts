ec1fe231313767cb865761d90bdbb97a8407bc37
Robin Punnoose <robin.punnoose@protonmail.com>
2023-10-10 03:34:59 -0500

    added test season start, commissioner

--- backend/test/Fantasy.ts
+++ backend/test/Fantasy.ts
@@ -18,8 +18,36 @@ describe("Deployment", function () {
   it("Should deploy the contract", async function () {
     const { fantasyContract } = await loadFixture(deployContractFixture);
 
-    console.log(`Contract deployed to: ${fantasyContract.target}`);
+    // console.log(`Contract deployed to: ${fantasyContract.target}`);
 
     expect(fantasyContract.target).to.not.equal("0x0");
+  });
+
+  it("Should start a season", async function () {
+    const { fantasyContract } = await loadFixture(deployContractFixture);
+    const amount = ethers.parseEther("1");
+
+    await fantasyContract.addSeason(amount);
+
+    const seasonStarted = await fantasyContract.seasonCounter();
+    // console.log(`Season counter: ${seasonStarted}`);
+
+    expect(seasonStarted).to.equal(1);
+  });
+
+  it("Should be the commisioner", async function () {
+    const { fantasyContract, commissioner } = await loadFixture(
+      deployContractFixture
+    );
+    const amount = ethers.parseEther("1");
+
+    await fantasyContract.addSeason(amount);
+
+    const commish = await fantasyContract.getSeasonCommissioner(0);
+    console.log(
+      `Season commish: ${commish}\n Commisioner: ${commissioner.address} `
+    );
+
+    expect(commissioner.address).to.equal(commish);
   });
 });
